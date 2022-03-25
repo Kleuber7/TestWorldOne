1bdeee32b26830d57ee741b97b0b580e7a8c6be4
Kleuber <kleubercardoso@outlook.com>
2022-03-25 20:46:27 -0300

    att crew

--- TCC/Assets/Scripts/Inimigos/Crew.cs
+++ TCC/Assets/Scripts/Inimigos/Crew.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class Crew : MonoBehaviour
 {
-    [SerializeField] List<INIPerseguir> crews;
-    
+    [SerializeField] public List<INIPerseguir> crews;
+    public bool checkCrew;
 
     public void CallCrew(Transform target)
     {
-        foreach(INIPerseguir crew in crews)
+        foreach (INIPerseguir crew in crews)
         {
             crew.alvo = target;
             crew.Perseguir();
             crew.inCrew = true;
+            checkCrew = true;
         }
+    }
+
+    public void CheckCrew()
+    {
+        foreach (INIPerseguir crew in crews)
+        {
+            if (crew.inCrew)
+            {
+                if (Vector3.Distance(crew.transform.position, crew.alvo.position) < crew.areaVisao)
+                {
+                    checkCrew = true;
+                    break;
+                }
+                else
+                {
+                    checkCrew = false;
+                }
+            }
+        }
+
+        if (!checkCrew)
+        {
+            foreach (INIPerseguir crew in crews)
+            {
+                crew.NaoPerseguir();
+                crew.inCrew = false;
+            }
+        }
+
     }
 }
